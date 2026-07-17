7bcdbd610b868523775be9c066a5685b4cea3330
Mustopha Abdulqadir <mustophaqadir@gmail.com>
2026-07-17 20:03:35 +0100

    feat: add database utility scripts for partner migration and data clearing, and configure Cloudinary image patterns in next.config.ts

--- ifip-frontend/next.config.ts
+++ ifip-frontend/next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  assetPrefix: process.env.NODE_ENV === "production" ? "https://ifip.nextif.org" : undefined,
   images: {
     remotePatterns: [
       {
